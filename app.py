bf5b5aa224d2b545df5ab6144c49792dd0eaec6c
javier <estudiopena2017@gmail.com>
2025-10-22 16:09:37 -0300

    Configuro Flask para servir frontend desde /

--- app.py
+++ app.py
@@ -59,3 +59,15 @@ def serve_index():
 def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='../frontend', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
