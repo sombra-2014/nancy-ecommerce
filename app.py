87b2cc393aaadfa2fc2df843703cbddb72958d15
javier <estudiopena2017@gmail.com>
2025-10-21 21:06:11 -0300

    Proyecto Flask listo para Render

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
-    return "Hola desde Nancy!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    return "¡Hola desde Nancy en Render!"
