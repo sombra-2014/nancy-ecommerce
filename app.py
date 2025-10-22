b60680d831d9a2d6786d5b7ad873a2f1ba687025
javier <estudiopena2017@gmail.com>
2025-10-22 14:09:25 -0300

    Importar jsonify para API del carrusel

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, jsonify
 from flask import Flask, render_template, send_from_directory
 import os
 
@@ -10,3 +11,13 @@ def index():
 @app.route("/<path:filename>")
 def static_files(filename):
     return send_from_directory(app.static_folder, filename)
+
+
+
+@app.route('/api/carousel_images')
+def get_carousel_images():
+    return jsonify([
+        {"url": "/static/img/carrusel1.jpg", "alt": "Imagen 1"},
+        {"url": "/static/img/carrusel2.jpg", "alt": "Imagen 2"},
+        {"url": "/static/img/carrusel3.jpg", "alt": "Imagen 3"}
+    ])
