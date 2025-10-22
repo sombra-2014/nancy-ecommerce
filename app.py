e124fc2cf35cb1dbbb52ddeb96604f19f7cecb16
javier <estudiopena2017@gmail.com>
2025-10-22 15:52:40 -0300

    Configuro Flask para servir index.html desde /

--- app.py
+++ app.py
@@ -30,3 +30,15 @@ app = Flask(__name__, static_folder="../frontend", static_url_path="")
 @app.route("/")
 def serve_index():
     return send_from_directory(app.static_folder, "index.html")
+
+    
+
+
+    from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='../frontend', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
